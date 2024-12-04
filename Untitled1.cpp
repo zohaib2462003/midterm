43985045e91066561982fc23d69ea4e4d861152c
zohaib2462003 <zohaibaptech24@gmail.com>
2024-12-04 09:45:10 +0500

    done

---
+++ Untitled1.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+
+int main() {
+ int check balance; deposit money;withdraw money; exit; 
+double balance
+	cout<< "check balance"<<end1;
+	cout<< "deposit money"<<end1;
+	cout<< "withdraw money"<<end1;
+	cout<<"exit"<<end1;
+	
+	switch(choice)
+	case 1 :
+		cout<<"this is your"<<balance;
+		break;
+		
+		case 2 : 
+		cout<<"enter the deposit amount"<<;
+		
+		case 3 :
+		cout<< "enter the withdrwal amount"<<;
+			
+case 4:
+cout<< "exit"<<;	
+	
+	do { 
+	cout<<"deposit"<<
+	break;
+	cout<<"withdraw"<<
+	break;
+	cout<<"balance-check"<<
+	break;
+	cout<<"exit"<<
+	}
+	if{"deposit"( "enter the deposit amount")end1;
+	break;
+	if( "withdraw" ("enter the withdrawal amount"))
+	break;
+	while ("balance=0")}
+	
+	
+
+	return 0;
+	
+}
+
